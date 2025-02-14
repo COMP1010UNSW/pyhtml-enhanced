c4c8963723d0f87d8a3f1dd71c6db6ff591c9b65
Maddy Guthridge <hello@maddyguthridge.com>
2025-02-14 15:45:40 +1100

    Exclude base classes from code gen tests

--- tests/code_gen_test.py
+++ tests/code_gen_test.py
@@ -5,23 +5,34 @@ Tests for code generation
 
 These don't regenerate the code, but rather test code that has been generated
 """
+
 import pytest
 
 import pyhtml
-from pyhtml import Comment, DangerousRawHtml, Tag, html
+from pyhtml import (
+    Comment,
+    DangerousRawHtml,
+    SelfClosingTag,
+    Tag,
+    WhitespaceSensitiveTag,
+    html,
+)
 
 all_tags = [
     getattr(pyhtml, i)
     for i in dir(pyhtml)
     if (
         # Ignore __dunder__ items
-        not i.startswith('__')
+        not i.startswith("__")
         # Only look at classes
         and type(getattr(pyhtml, i)) is type
         # That are a kind of Tag
         and issubclass(getattr(pyhtml, i), Tag)
-        # And aren't a PyHTML feature (since comments require named args)
+        # Aren't a PyHTML-specific feature
         and not issubclass(getattr(pyhtml, i), Comment | DangerousRawHtml)
+        # Aren't a base class
+        and getattr(pyhtml, i)
+        not in [Tag, SelfClosingTag, WhitespaceSensitiveTag]
         # And isn't <html> since it has pre-content
         and not issubclass(getattr(pyhtml, i), html)
     )
@@ -33,13 +44,10 @@ def test_num_exported_members():
     We don't want regenerating the code to produce any more (or fewer) members
     """
     # Just update the number if you're expecting it to change
-    assert len(all_tags) == 117
+    assert len(all_tags) == 114
 
 
-@pytest.mark.parametrize(
-    'tag',
-    all_tags
-)
+@pytest.mark.parametrize("tag", all_tags)
 def test_tag_renders(tag: type[Tag]):
     """
     Test that each tag correctly renders
@@ -49,10 +57,7 @@ def test_tag_renders(tag: type[Tag]):
     assert str(tag()).startswith(f"<{tag()._get_tag_name()}")
 
 
-@pytest.mark.parametrize(
-    'tag',
-    all_tags
-)
+@pytest.mark.parametrize("tag", all_tags)
 def test_tag_callable(tag: type[Tag]):
     """
     Test that each tag correctly renders after calling itself
